54b486289fed1cebb6d1afaeda3296dd3112e7ef
teaghan <obriaintb@gmail.com>
2025-03-20 15:34:21 -0700

    removed copy button

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-equation-editor",
-    version="0.2.0",
+    version="0.3.0",
     author="Teaghan O'Briain",
     author_email="obriaintb@gmail.com",
     description="A WYSIWYG equation editor component for Streamlit",
